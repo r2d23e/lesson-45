97d7bef8794063b1d41ebd629ddc6fc886c034bb
r2d23e <kildiyarov.timur13@gmail.com>
2023-03-23 12:02:01 +0500

    cw

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
 let Masiv = [
     {
         name: "Saidbek",
-        age:13,
+        age: 13,
 
-    },   {
+    }, {
         name: "Odik",
-        age:13,
-        
+        age: 13,
+
     }, {
         name: "Pasha",
-        age:32,
-        
-    },  {
+        age: 32,
+
+    }, {
         name: "Alisher",
-        age:42,
-        
+        age: 42,
+
     },
 ]
 
-console.log(Masiv);
+let ages = []
+for (let item of Masiv) {
+    if (item.age >= 20) {
+        ages.push(item)
+    }
+}
+let filt = Masiv.filter(item => item.age >= 20)
+for (let item of Masiv) {
+    item.income = item.age + 200
+}
+
+console.log(filt);
+console.log(ages);
+
+
+
+
+let users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+users = users.sort((a, b) => a - b)
+
+let user2 = users.map
+
+console.log();
