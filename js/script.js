5394b60bd89746384e36bc52e3265cbf4655e30c
r2d23e <117255103+r2d23e@users.noreply.github.com>
2023-03-23 11:20:52 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let Masiv = [
+    {
+        name: "Saidbek",
+        age:13,
+
+    },   {
+        name: "Odik",
+        age:13,
+        
+    }, {
+        name: "Pasha",
+        age:32,
+        
+    },  {
+        name: "Alisher",
+        age:42,
+        
+    },
+]
+
+console.log(Masiv);
