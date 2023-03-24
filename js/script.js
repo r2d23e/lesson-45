638fec49dc46d9d291bcff5836d59fe1c1aaeadc
r2d23e <kildiyarov.timur13@gmail.com>
2023-03-24 13:40:07 +0500

    cw

--- js/script.js
+++ js/script.js
@@ -47,15 +47,30 @@ console.log(find_el);
 let filt = Masiv.filter(item => item.age >= 20)
 console.log(filt);
 //-------------------------------//
+let new_arr = Masiv.map((item) => {
+    return item.age + 200
+})
+console.log(new_arr);
+//-------------------------------//
+let new_arr2 = Masiv.map(item => item.age + 200)
+console.log(new_arr2);
+//-------------------------------//
 
 
 
-
-
-
+//-------------------------------//
 let users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 users = users.sort((a, b) => a - b)
-
-let user2 = users.map
-
-console.log();
+let user2 = users.map(item => item - 2)
+console.log(users, user2);
+//-------------------------------//
+let user3 = users.some(item => item == 2)
+console.log(user3);
+//-------------------------------//
+user3 = users.every(item => item == 2)
+console.log(user3);
+//-------------------------------//
+users.forEach(item => {
+    item = item + 2
+})
+console.log(users);
